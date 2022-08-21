b7a22f052a6f8d0787a42b7e2176653345b1d3e1
Vladimir <kruglow.vova@gmail.com>
2022-08-21 17:13:35 +0300

    DeployHeroku

--- scraping/scraping/settings/production.py
+++ scraping/scraping/settings/production.py
@@ -39,7 +39,7 @@ SECRET_KEY = SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["scrapingswork.herokuapp.com"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
